e62868b14f2b24e592cb25d8d141cfbb2f4d814e
Frank tha Tank <fraoh@yahoo.com>
2018-08-25 19:54:11 -0700

    split

---
+++ convert.py
@@ -0,0 +1,3 @@
+from os import system
+
+system('convert -delay 10 -loop 0 image*.jpg animation.gif')
--- timelapse.py
+++ timelapse.py
@@ -4,9 +4,9 @@ from time import sleep
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
+stop = False
 
-for i in range(10):
+while not stop
+#for i in range(60):
     camera.capture('image{0:04d}.jpg'.format(i))
     sleep(60)
-
-system('convert -delay 10 -loop 0 image*.jpg animation.gif')
