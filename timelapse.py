2f8961d2794a4527c6543e2b8f7e92521cba7ab8
Frank tha Tank <fraoh@yahoo.com>
2018-08-25 19:58:24 -0700

    cleanup

--- timelapse.py
+++ timelapse.py
@@ -1,12 +1,11 @@
 from picamera import PiCamera
-from os import system
 from time import sleep
 
 camera = PiCamera()
 camera.resolution = (1024, 768)
 stop = False
 
-while not stop
+while not stop:
 #for i in range(60):
     camera.capture('image{0:04d}.jpg'.format(i))
     sleep(60)
